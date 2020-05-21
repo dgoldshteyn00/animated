b598cc604e248a3cf8eaf582dc2d8b318b0957c0
dgoldshteyn00 <45824229+dgoldshteyn00@users.noreply.github.com>
2020-05-21 10:05:54 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from pip._vendor.distlib.compat import raw_input
+
+from script import run
+import sys
+
+if len(sys.argv) == 2:
+    run(sys.argv[1])
+elif len(sys.argv) == 1:
+    run(raw_input("please enter the filename of an mdl script file: \n"))
+else:
+    print("Too many arguments.")
